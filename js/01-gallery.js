5421300f3f6ff31814838f2031eb177c515cb037
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-09-25 11:54:57 +0300

    add lazyLoading to task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,8 +10,8 @@ function createGalleryMarkup(galleryItems) {
     .map(({ preview, original, description }) => {
       return `
         <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-        <img class="gallery__image" data-source="${original}" src="${preview}" alt="${description}"></img>
+        <a class="gallery__link"  href="${original}">
+        <img loading="lazy" class="gallery__image lazyload" data-source="${original}" data-src="${preview}" alt="${description}"></img>
         </a>
         </div>
         `;
@@ -45,4 +45,48 @@ function onCloseWscModal(event) {
     setTimeout(() => modalContainer.remove(), 300);
     window.removeEventListener("keydown", onCloseWscModal);
   }
+}
+
+const lazyImage = document.querySelectorAll("img[data-src]");
+
+console.log(lazyImage);
+
+lazyImage.forEach((image) => {
+  image.addEventListener("load", onImageLoad, { once: true });
+});
+
+function onImageLoad(evt) {
+  console.log("Image loaded");
+  evt.target.classList.add("appear");
+}
+
+// ==========+++++++Feature detection++++++++++======
+
+if ("loading" in HTMLImageElement.prototype) {
+  // supported in browser
+  console.log("Браузер поддерживает Ленивая загрузка ");
+  addLazyImagesSrc();
+} else {
+  // fetch polyfill/third-party library
+  console.log("Браузер не поддерживает Ленивая загрузка ");
+  addLazyLoadingScript();
+}
+
+function addLazyImagesSrc() {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+  lazyImages.forEach((img) => {
+    img.src = img.dataset.src;
+  });
+}
+
+function addLazyLoadingScript() {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossOrigin = "anonymous";
+  script.referrerPolicy = "no-referrer";
+
+  document.body.appendChild(script);
 }
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,65 +1,170 @@
 export const galleryItems = [
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
+    description: "Hokkaido Flower",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=201909/640x480/look.com.ua-354060.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=201909/1280x960/look.com.ua-354060.jpg",
+    description: "BMW",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=202209/640x480/look.com.ua-393679.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=202209/1280x1024/look.com.ua-393679.jpg",
+    description: "river",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=201902/640x480/look.com.ua-330591.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=201902/1280x1024/look.com.ua-330591.jpg",
+    description: "cats",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=201209/640x480/look.com.ua-24262.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=201209/1280x1024/look.com.ua-24262.jpg",
+    description: "markers",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=201901/640x480/look.com.ua-320769.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=201901/1280x1024/look.com.ua-320769.jpg",
+    description: "old map",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=201901/640x480/look.com.ua-320657.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=201901/1280x1024/look.com.ua-320657.jpg",
+    description: "train",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=202103/640x480/look.com.ua-375515.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=202103/1280x1024/look.com.ua-375515.jpg",
+    description: "flowers",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=202008/640x480/look.com.ua-357648.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=202008/1280x1024/look.com.ua-357648.jpg",
+    description: "brilliants",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=201512/640x480/look.com.ua-140529.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=201512/1280x1024/look.com.ua-140529.jpg",
+    description: "slipping dog",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=202208/640x480/look.com.ua-391672.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=202208/1280x1024/look.com.ua-391672.jpg",
+    description: "mushroom",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=202209/640x480/look.com.ua-393219.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=202209/1280x1024/look.com.ua-393219.jpg",
+    description: "bike",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=202206/640x480/look.com.ua-388558.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=202206/1280x1024/look.com.ua-388558.jpg",
+    description: "winter",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=201405/640x480/look.com.ua-103386.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=201405/1280x1024/look.com.ua-103386.jpg",
+    description: "flag",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=202209/640x480/look.com.ua-393645.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=202209/1280x1024/look.com.ua-393645.jpg",
+    description: "fruits",
+  },
+  {
+    preview:
+      "https://www.look.com.ua/download.php?file=202209/640x480/look.com.ua-393478.jpg",
+    original:
+      "https://www.look.com.ua/download.php?file=202209/1280x1024/look.com.ua-393478.jpg",
+    description: "bread",
   },
 ];
